51c1bf983456f38e4695f66be63f73f83d820396
Edwin Lee <leeed2001@gmail.com>
2026-06-26 10:16:05 -0500

    Update storm check and bump version to 1.0

--- tray_weather/storms.py
+++ tray_weather/storms.py
@@ -45,6 +45,7 @@ class StormManager:
         self.longitude = longitude
 
     def icon_color(self, test_type: int | None = None):
+        self.get_watch_warnings()
         type_to_check = test_type if test_type else self.storm_type
         if type_to_check == StormType.FloodWatch:
             return 'powderblue'
